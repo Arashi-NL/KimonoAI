3c14d886febd870119bd251260257f2e9cb87916
Vincent van Breukelen <x.vinnievengeance@hotmail.com>
2018-05-08 15:50:14 +0200

    Second Commit (corrected)

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
-import os, numpy
+import os
+import numpy as np
 from skimage import data as sd
+import matplotlib.pyplot as plt
 
 def load_data(data_directory):
     directories = [d for d in os.listdir(data_directory)
@@ -19,8 +21,17 @@ def load_data(data_directory):
             labels.append(int(d))
         return images, labels
 
-#ROOT_PATH = "E:\Coding\Data (Belgian Traffic Signs)"
-train_data_directory = os.path.join("E:\Coding\Data (Belgian Traffic Signs)\Training")
-test_data_directory = os.path.join("E:\Coding\Data (Belgian Traffic Signs)\Testing")
+ROOT_PATH = "\Coding\TrafficSigns"
+train_data_directory = os.path.join(ROOT_PATH, "Training")
+test_data_directory = os.path.join(ROOT_PATH, "Testing")
 
 images, labels = load_data(train_data_directory)
+
+images2 = np.array(images)
+labels2 = np.array(labels)
+print(images2.ndim)
+print(images2.size)
+images[0]
+
+plt.hist(labels2, 62)
+plt.show
